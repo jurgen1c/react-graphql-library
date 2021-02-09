6185c47b311b3226a107f62921c5829024f7ec1b
jurgen1c <jurgen1c@gmail.com>
2021-02-08 21:43:00 -0600

    Add BookList, Book components & Setup apollo

---
+++ client/src/components/Book.js
@@ -0,0 +1,9 @@
+const Book = () => {
+  return (
+    <div>
+      <h3>Book Title</h3>
+    </div>
+  )
+}
+
+export default Book;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import Book from './Book'
+
+const BookList = () => {
+  return(
+    <div>
+      <ul className='bookList'>
+        <Book/>
+      </ul>
+    </div>
+  )
+}
+
+export default BookList;
